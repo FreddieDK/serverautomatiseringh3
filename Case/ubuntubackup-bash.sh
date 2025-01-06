adfd8f3e5a7cca4f5ee45120c0cae2ebc836688f
sofnoclip <frederik@gyldenstrand.dk>
2025-01-06 12:43:57 +0100

    Update ubuntubackup-bash.sh

--- Case/ubuntubackup-bash.sh
+++ Case/ubuntubackup-bash.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-# Backupped filer
-backup="/home /var/spool/mail /etc /root /boot /opt"
+# Backup source: Everything from root directory except excluded folders
+backup="/"
+
+# Exclude directories (e.g., /mnt, /proc, /sys, /dev, etc.)
+exclude_dirs=("--exclude=/mnt")
 
 # Destination
 dest="/mnt/backup/serverbackups"
 
-# Opret arkiveret fil med tidspunkt/dato
+# Create archive filename with date/time
 tid=$(date +%d-%m-%Y)
 hostname=$(hostname -s)
 archive_fil="$hostname-$tid.tgz"
 
-# Print besked I console
-echo "Backup op af $backup til $dest/$archive_fil"
+# Print message to console
+echo "Backing up $backup to $dest/$archive_fil"
 date
 echo
 
-# Arkiver/zip fil 
-tar czf $dest/$archive_fil $backup
+# Archive/zip files while excluding specified directories
+tar czf $dest/$archive_fil "${exclude_dirs[@]}" $backup
 
-# Print status besked
+# Print status message
 echo
 echo "Backup finished"
 date
 
-# slet filer ældre end 14 dage 
+# Delete files older than 14 days
 find /mnt/backup/serverbackups -type f -mtime +14 -delete
