02a90f9294a24629ebb2da549c67ef19b2c2c8a4
Freddie <46194722+FreddieDK@users.noreply.github.com>
2025-01-08 09:19:20 +0100

    yderlig ændring til man

--- Case/backupscript-linux.sh
+++ Case/backupscript-linux.sh
@@ -3,7 +3,7 @@
 # Script til backup af filer på bash CLI'er
 
 # Man-page // help 
-MANPATH /backup.man
+MANPATH backupscript-linux.man
 
 # Variabel til standard source af filer 
 standard_kilde="/home"
