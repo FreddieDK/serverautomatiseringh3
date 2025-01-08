1852b5c1e1ed3312df3da78c0e61e91f18fd149b
Freddie <46194722+FreddieDK@users.noreply.github.com>
2025-01-08 13:04:05 +0100

    Update ekstra-opg.sh

--- Case/ekstra-opg.sh
+++ Case/ekstra-opg.sh
@@ -1,56 +1,55 @@
-#!/bin/bash
+    #!/bin/bash
 
-# Funktion til at lave ping
-function pingnow(){
-read -p "Hvad vil du tjekke? (Ping IP el. Domain): " pingvalg
-    if ping -c 1 $pingvalg > /dev/null; then
-        echo "Server svare!"
+    # Funktion til at lave ping
+    function pingnow(){
+    read -p "Hvad vil du tjekke? (Ping IP el. Domain): " pingvalg
+        if ping -c 1 $pingvalg > /dev/null; then
+            echo "Server svare!"
+        else
+            echo "Server svare ikke.."
+        fi
+    }
+
+    # Funktion til at lave nslookup
+    function nslookupnow(){
+    read -p "Hvilket domain vil du tjekke?: " domainvalg
+    read -p "Hvillken dns server vil du bruge?: (Standard 1.1.1.1) " dnsvalg
+        if [[ $dnsvalg == null ]]; then
+            nslookup $domainvalg 1.1.1.1 
+        else
+            nslookup $domainvalg $dnsvalg
+        fi
+    }
+
+    #Funktion til at lave tracert
+    function tracertnow(){
+        echo "test"
+    }
+
+    # Besked ved kørsel af script
+
+    echo "Hvad vil du gerne teste?:"
+
+    echo "1 - Tjek server-status (Ping en IP eller domain)"
+    echo "2 - Nslookup (Tjek om dns fungere)"
+    echo "3 - Tracert (Tjekke om fejl på routning til en bestemt lokation)"
+    echo "4 - Alle (Køre alle funktioner i script)"
+
+    read valg
+
+    # Valg af funktion-kørsel
+
+    if [[ "$valg" == "1" ]]; then
+        pingnow
+    elif [[ "$valg" == "2" ]]; then
+        nslookupnow
+    elif [[ "$valg" == "3" ]]; then
+        tracertnow
+    elif [[ "$valg" == "4" ]]; then
+        pingnow
+        nslookupnow
+        tracertnow
     else
-        echo "Server svare ikke.."
-    fi
-}
-
-# Funktion til at lave nslookup
-function nslookupnow(){
-read -p "Hvilket domain vil du tjekke?: " domainvalg
-read -p "Hvillken dns server vil du bruge?: (Standard 1.1.1.1) " dnsvalg
-    if [[ $dnsvlag == null ]]; then
-        nslookup $domainvalg 1.1.1.1 
-    else
-        nslookup $domainvalg dnsvalg
-    fi
-}
-
-# Funktion til at lave tracert
-#function tracertnow(){
-
-    
-#}
-
-# Besked ved kørsel af script
-
-echo "Hvad vil du gerne teste?:"
-
-echo "1 - Tjek server-status (Ping en IP eller domain)"
-echo "2 - nslookup (Tjek om dns fungere)"
-echo "3 - tracert (Tjekke om fejl på routning til en bestemt lokation)"
-echo "4 - Alle (Køre alle funktioner i script)"
-
-read valg
-
-# Valg af funktion-kørsel
-
-if [[ "$valg" == "1" ]]; then
-    pingnow
-elif [[ "$valg" == "2" ]]; then
-    nslookupnow
-elif [[ "$valg" == "3" ]]; then
-    tracertnow
-elif [[ "$valg" == "4 "]]; then
-    pingnow
-    nslookupnow
-    tracertnow
-else
-    echo "Dette er ikke en gyldig valgmulighed"
-fi 
+        echo "Dette er ikke en gyldig valgmulighed"
+    fi 
 
