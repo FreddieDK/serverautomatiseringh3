23ef912b3a97d125813259e39807bf2e54cfa250
Freddie <46194722+FreddieDK@users.noreply.github.com>
2025-01-08 10:27:57 +0100

    Oprettelse af funktioner og echos

--- Case/ekstra-opg.sh
+++ Case/ekstra-opg.sh
@@ -1,1 +1,46 @@
 #!/bin/bash
+
+echo "Hvad vil du gerne teste?:"
+
+echo "1 - Ping (Ping en IP eller domain)"
+echo "2 - nslookup (Tjek om dns fungere)"
+echo "3 - tracert (Tjekke om fejl på routning til en bestemt lokation)"
+
+read valg
+
+
+function pingnow(){
+
+
+}
+
+function nslookupnow(){
+
+    
+}
+
+function tracertnow(){
+
+    
+}
+
+if [[ "$valg" == "1" ]]; then
+    pingnow
+elif [[ "$valg" == "2" ]]; then
+    nslookupnow
+elif [[ "$valg" == "3" ]]; then
+    tracertnow
+else
+    echo "Dette er ikke en gyldig valgmulighed"
+fi 
+
+
+
+
+
+
+
+
+
+
+}
