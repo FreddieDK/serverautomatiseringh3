1c6fdedae30753870d9b72cb3d8877d2947ba94c
PiroPG <madssvinther@gmail.com>
2025-01-10 09:01:11 +0100

    Update Test-Server-Status.ps1

--- Case/Test-Server-Status.ps1
+++ Case/Test-Server-Status.ps1
@@ -4,26 +4,40 @@ $computername = "LAPTOP-I651QOV4"
     Get-DiskUtilization -computername $computername
 
     function Get-CpuUtilization {
-    
-    Get-WmiObject Win32_processor -ComputerName $computername |
-    Select-Object Name, LoadPercentage, NumberOfCores |
-    Format-Table -AutoSize
+    try {
+        Get-WmiObject Win32_processor -ComputerName $computername |
+        Select-Object Name, LoadPercentage, NumberOfCores |
+        Format-Table -AutoSize
+    }
+    catch {
+        <#Do this if a terminating exception happens#>
+    }
     }
     
     function Get-MemoryUtilization {
 
-    Get-WmiObject -Class Win32_ComputerSystem -ComputerName $computername |    
-    Select-Object PSComputername, @{Name="FreeMemoryGB";Expression={[math]::Round(($_.FreePhysicalMemory/1GB),2)}} |
-    Format-Table -AutoSize
+    try {
+        Get-WmiObject -Class Win32_ComputerSystem -ComputerName $computername |    
+        Select-Object PSComputername, @{Name="FreeMemoryGB";Expression={[math]::Round(($_.FreePhysicalMemory/1GB),2)}} |
+        Format-Table -AutoSize
 
-    Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computername |
-    Select-Object PSComputername, @{Name="TotalMemoryGB";Expression={[math]::Round(($_.TotalVisibleMemorySize/1MB),2)}} |
-    Format-Table -AutoSize
+        Get-WmiObject -Class Win32_OperatingSystem -ComputerName $computername |
+        Select-Object PSComputername, @{Name="TotalMemoryGB";Expression={[math]::Round(($_.TotalVisibleMemorySize/1MB),2)}} |
+        Format-Table -AutoSize
+    }
+    catch {
+        <#Do this if a terminating exception happens#>
+    }
     }
     
     function Get-DiskUtilization {
 
-    Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computername |
-    Select-Object SystemName, DeviceID, MediaType, @{Name="SizeGB";Expression={[math]::Round(($_.Size/1GB),2)}}, @{Name="FreeSpaceGB";Expression={[math]::Round(($_FreeSpace/1GB),2)}} |
-    Format-Table -AutoSize
+    try {
+        Get-WmiObject -Class Win32_LogicalDisk -ComputerName $computername |
+        Select-Object SystemName, @{Name="SizeGB";Expression={[math]::Round(($_.Size/1GB),2)}}, @{Name="FreeSpaceGB";Expression={[math]::Round(($_FreeSpace/1GB),2)}} |
+        Format-Table -AutoSize
+    }
+    catch {
+        <#Do this if a terminating exception happens#>
+    }
     }
